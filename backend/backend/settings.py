d9dbd8821f0a8c41c4ed6a4867c6d1e2c183383f
RobertTate <robertjosephtate@gmail.com>
2021-02-06 14:51:25 -0600

    add serializer and set new key with environment variable

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a+&%^9n+#j&^k^=_h1f(u=dgu$87i9y_@)6!y^iw7r$tq#&64_'
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
